3f54ef6d627bfde8cd31a92642f0856bfcc86bf5
JULIANO <143232895+julianorei@users.noreply.github.com>
2024-03-18 16:08:25 +0000

    Update __resource.lua

--- juliano_orgs/__resource.lua
+++ juliano_orgs/__resource.lua
@@ -6,9 +6,9 @@ repository 'https://github.com/julianorei/juliano_orgs'
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 autor "julianorei#9410"
-description 'Sistema de Organizações do Juliano'
+description 'Sistema de Organizações do Juliano Utilizado no Summer RP ;)'
 
-version '0.1.1'
+version '0.1.0'
 
 server_scripts {
   '@es_extended/locale.lua',
